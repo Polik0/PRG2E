222a7f30bce7206fcd1e06e19ad45ce387897764
p.polkovnychenko <ppolina0404@gmail.com>
2024-01-29 10:45:18 +0100

    changes

---
+++ src/multiArrays/multiArrays.java
@@ -0,0 +1,14 @@
+package multiArrays;
+
+public class multiArrays {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        int[] simpleArray = new int[5];
+
+        int[][] twoArrays = {
+                {5, 6, 8},
+                {5, 2, 1},
+                {3, 3, 6}
+        };
+    }
+}
